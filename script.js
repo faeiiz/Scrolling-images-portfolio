ce59fa49021570020a2e29581354e2bf2daa856c
Faiz Mansuri <faizmansuri5@gmail.com>
2024-11-16 23:20:53 +0530

    1st

---
+++ script.js
@@ -0,0 +1,52 @@
+// Initialize Lenis
+const lenis = new Lenis({
+  duration: 2,
+});
+function raf(time) {
+  lenis.raf(time);
+  requestAnimationFrame(raf);
+}
+
+requestAnimationFrame(raf);
+
+document.querySelectorAll(".elem").forEach((elem) => {
+  let image = elem.querySelector("img");
+  let tl = gsap.timeline();
+
+  let random = gsap.utils.random(-100, 100);
+
+  tl.set(
+    image,
+    {
+      transformOrigin: `${random < 0 ? 0 : "100%"}`,
+    },
+    "start"
+  )
+    .to(
+      image,
+      {
+        scale: 0,
+        ease: "none",
+        scrollTrigger: {
+          trigger: image,
+          start: "top top",
+          end: "bottom top",
+          scrub: true,
+          markers: true,
+        },
+      },
+      "start"
+    )
+    .to(elem, {
+      xPercent: random,
+      ease: "none",
+      scrollTrigger: {
+        trigger: image,
+        start: "top bottom",
+        end: "bottom top",
+        scrub: true,
+      },
+    });
+});
+
+console.log(document.querySelectorAll(".elem"));
